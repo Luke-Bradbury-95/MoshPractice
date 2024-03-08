f585ac07424ee38000d7cb8c3c47963a31e3f5da
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-03-08 21:36:07 +0000

    Added some classes and multiple constructors

--- Cars.cs
+++ Cars.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MoshPractice
 {
     internal class Cars
     {
+        private string _name;
+        private int _hp;
+        private string _color;
+
+        
+
+        // Default Constructor
+
         public Cars()
         {
-            Console.WriteLine("Car was created");
+            Console.WriteLine(_name + " was created");
+            _name = "Car";
+            _hp = 0;
+            _color = "Red";
 
+        }
+
+        //Partial Specific Constructor
+        public Cars(string name, int hp = 0, string color = "black")
+        {
+            Console.WriteLine(name + " was created");
+            _name = name;
+            _hp = hp;
+            _color = color;
+        }
+
+        //Specific COnstructor 
+
+
+
+        public void Drive()
+        {
+            Console.WriteLine(_name + " is now driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine(_name + " has stopped driving");
+        }
+
+        public void Description()
+        {
+            Console.WriteLine("The" + _color + " " + _name + " has " + _hp + " Horsepower");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,57 @@
 ﻿using MoshPractice;
+using System.Security.AccessControl;
+// Object creating
+Cars bmw = new Cars("Bmw", 350, "Red");
+bmw.Drive();
+Cars audi = new Cars("Audi", 450, "Blue");
+audi.Drive();
+audi.Description();
+bmw.Description();
 
-Cars BMW = new Cars();  
+Console.WriteLine("Press 1 to stop");
+string userInput = Console.ReadLine();
+if(userInput == "1")
+{
+    bmw.Stop();
+}
+else if(userInput == "2")
+{
+    audi.Stop();
+}
+else
+{
+    {
+        Console.WriteLine("Car drives forever");
+    }
+}
+Console.ReadKey();
+
+
+
+
+public class Phone
+{
+    public string Company;
+    public string Model;
+    public string ReleaseDay;
+
+    public Phone()
+    {
+        Company = "unknown";
+        Model = "unknown";
+        ReleaseDay = "unknown";
+        }
+
+    public Phone(string company, String model, String releaseDay = "unknown")
+    {
+        Company = company;
+        Model = model;
+        ReleaseDay = releaseDay;
+    }
+
+    public Phone(string company, String model, String releaseDay)
+    {
+        Company = company;
+        Model = model;
+        ReleaseDay = releaseDay;
+    }
